55caf5d7c39d662ccdbd923b16c10a2f6e89940f
Shazmaan <shazmaan@buffalo.edu>
2019-07-24 12:29:50 -0400

    Finished Problem2.

---
+++ Problm1.cpp
@@ -0,0 +1,28 @@
+//
+// Created by shazm on 7/24/2019.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution{
+public:
+
+};
+
+int main(){
+    Solution s;
+
+    return 0;
+}
