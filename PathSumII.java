b6e6f1ee7ff6bc3f845243f84c231f94bdb68b45
Manogna Mujje <manogna.mujje@gmail.com>
2019-08-21 16:29:07 -0700

    Completed PathSum using Backtracking

--- PathSumII.java
+++ PathSumII.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Time: O(n)
- * Space: O(n)
+ * Time: O(2^Depth of the tree) 
+ * No. of paths checked during the tree traversal
+ * Space: O(Depth of the tree)
  * Leetcode Accepted: yes
- * Problems face: No
+ * Problems face: Understanding the backtracking implementation in code for this problem was slightly difficult
  */
 public class PathSumII {
 
@@ -23,8 +24,46 @@ public class PathSumII {
 		root.right.right.left = new TreeNode(5);
 		root.right.right.right = new TreeNode(1);
 		System.out.println(pathSum(root, 22));
+		System.out.println(optimizedPathSum(root, 22));
 	}
 	
+	/**
+	 * Optimizing PathSum solution not to use copy of new ArrayList on every child node
+	 * Tip: Done using Backtracking --- Improved the runtime and space
+	 * Time: O(2^Depth of the tree) 
+	 */
+	
+	private static List<List<Integer>> optimizedPathSum(TreeNode root, int sum) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		List<Integer> set = new ArrayList<Integer>();
+		findPaths(root, sum, set, result);
+		return result;
+	}
+
+	private static void findPaths(TreeNode root, int sum, List<Integer> set, List<List<Integer>> result) {
+		if(root == null) return;
+		set.add(root.val);
+		
+		//Check if the root is a leaf node
+		//Since set is an ArrayList reference, and set varies through out the tree, it is better to have a copy of set
+		if(root.left == null && root.right == null && root.val == sum) 
+			result.add(new ArrayList<>(set));
+		
+		//Find paths on children nodes
+		findPaths(root.left, sum-root.val, set, result);
+		findPaths(root.right, sum-root.val, set, result);
+		
+		//Bactracking -- remove the current node from set
+		set.remove(set.size()-1);
+	}
+	
+	/**
+	 * Bruteforce solution: Passing a new copy of path(covered so far) set to every child node
+	 * @param root
+	 * @param sum
+	 * @return
+	 */
+
 	public static List<List<Integer>> pathSum(TreeNode root, int sum) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		if(root == null) return result;
