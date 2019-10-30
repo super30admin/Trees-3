06cd3eaf03410feb8d07a6e860a85e069bce1469
keerthitejakonuri <keerthi49100@gmail.com>
2019-10-29 23:28:20 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,7 +5,7 @@
 
 
 // Your code here along with comments explaining your approach
-class Solution {
+class Solution1 {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
         List<List<Integer>> result = new ArrayList<>();
         pathSumHelper(root,new ArrayList<>(),result,sum);
@@ -27,5 +27,24 @@ class Solution {
         pathSumHelper(root.left,leftList,result,sum-root.val);
         List<Integer> rightList = new ArrayList<>(leftList);
         pathSumHelper(root.right,rightList,result,sum-root.val);
+    }
+}
+
+//Time Complexity :O(N)
+//Space Complexity :O(1)+ recursion stack
+//Did this code successfully run on Leetcode :Yes
+//Any problem you faced while coding this :Nope. 
+
+
+//Your code here along with comments explaining your approach
+class Solution2 {
+    public boolean isSymmetric(TreeNode root) {
+     if(null == root){return true;}
+    return isSymmetricTree(root.left,root.right);
+    }
+    public boolean isSymmetricTree(TreeNode root1, TreeNode root2){
+        if(((null == root1) && (null == root2))){return true;}
+        if(((null == root1) && (null != root2)) || ((null == root2) && (null != root1))){return false;}
+        return root1.val == root2.val && isSymmetricTree(root1.left,root2.right)&&isSymmetricTree(root1.right,root2.left);  
     }
 }
