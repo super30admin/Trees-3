9be01c474565bd271253676be6eba5ca937e6c81
rutulusc <rutulbap@usc.edu>
2020-01-17 04:35:43 -0800

    Iterative solution for symmetric tree added

---
+++ prob47_iter.java
@@ -0,0 +1,18 @@
+// S30 Big N Problem #47 {Easy}
+// 101. Symmetric Tree
+// Time Complexity :
+// Space Complexity :  
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :None
+
+
+// Your code here along with comments explaining your approach
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
