6b9ef8063e599934a6629775c6964e6c182367c4
navnitss <43095390+navnitss@users.noreply.github.com>
2020-05-14 20:32:17 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,52 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(n^2)
+// Space Complexity :O(n^2)
+class Solution {
+     List<List<Integer>> result;
+    int target;
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        result=new ArrayList();
+        target=sum;
+        finder(root,0,new ArrayList<Integer>());
+        return result;
+    }
+    
+    void finder(TreeNode n,int tsum,List<Integer> arr)
+    {
+        if(n==null)
+            return;
+        arr.add(n.val);    
+        tsum+=n.val;
+        if(n.left==null&&n.right==null)
+        {
+            if(tsum==target)
+            {
+            result.add(new ArrayList<>(arr));
+            }
+        }
+        finder(n.left,tsum,arr);
+        finder(n.right,tsum,arr);
+        arr.remove(arr.size()-1);
+        
+    }
+}
+
+------------------------------------------------------------------------------
+
+// Time Complexity :O(n)
+// Space Complexity :O(n)
 
 
-// Your code here along with comments explaining your approach
+
+class Solution {
+   public boolean isSymmetric(TreeNode root) {
+    return isMirror(root, root);
+}
+
+public boolean isMirror(TreeNode t1, TreeNode t2) {
+    if (t1 == null && t2 == null) return true;
+    if (t1 == null || t2 == null) return false;
+    return (t1.val == t2.val)
+        && isMirror(t1.right, t2.left)
+        && isMirror(t1.left, t2.right);
+}
+}
