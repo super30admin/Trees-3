64fde47ebed905293dd5184eb48199a43fb219c1
albertcoder <eralbert9191@gmail.com>
2020-05-26 17:42:21 -0700

    recursive solution of symmetric binary tree

---
+++ symmetricTreeRecursive.java
@@ -0,0 +1,34 @@
+//time complexity O(n)
+//space complexity O(1)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right){
+        if(left == null && right == null) return true;
+        if(left == null && right != null) return false;
+        if(left != null && right == null) return false;
+            return
+                left.val == right.val &&
+                isSymmetric(left.right, right.left) &&
+                isSymmetric(left.left, right.right);
+    }
+}
