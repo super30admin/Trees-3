4747019ac2da1de28ea26e1ee2f2da484041b743
unknown <harale.s@husky.neu.edu>
2020-06-17 00:42:01 -0400

    Completed Symmetric Iterative approach

--- Symmetric.java
+++ Symmetric.java
@@ -21,6 +21,7 @@
  *     }
  * }
  */
+<----Recursive--->
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         //base
@@ -41,4 +42,69 @@ class Solution {
         
         return helper(left.left, right.right) && helper(left.right, right.left);
     }
+}
+
+<----Iterative--->
+// Time Complexity : O(N) where N number of nodes
+// Space Complexity : O(N)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class Pair {
+        TreeNode left;
+        TreeNode right;
+        
+        public Pair(TreeNode left, TreeNode right) {
+            this.left = left;
+            this.right = right;
+        }
+        
+        public TreeNode getKey() {
+            return this.left;
+        }
+        
+        public TreeNode getValue() {
+            return this.right;
+        }
+    }
+    public boolean isSymmetric(TreeNode root) {
+        //base
+        if(root == null){
+            return true;
+        }
+        
+        Stack<Pair> stack = new Stack<>();
+        stack.push(new Pair(root, root));
+        
+        while(!stack.isEmpty()){
+            Pair val = stack.pop();
+            TreeNode left = val.left;
+            TreeNode right = val.right;
+            
+            if(left != null || right != null){
+                if(left != null && right != null && left.val == right.val){
+                    stack.push(new Pair (left.left, right.right));
+                    stack.push(new Pair (left.right, right.left));    
+                }else{
+                    return false;
+                }           
+            }
+        }
+        return true;
+    }    
+    
 }
