e8d42096df4f2b8eb9bfaadd3e8e13ba9dedda67
karthikprabhu alagu <mail2karthi07@gmail.com>
2020-06-17 23:07:02 -0400

    Trees-3: Symmetric tree iterative approach.

--- SymmetricTree.swift
+++ SymmetricTree.swift
@@ -26,3 +26,41 @@ class Solution {
     }
     
 }
+
+//Iterative approach
+
+
+class Solution {
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        return isMirror(root)
+    }
+    
+  func isMirror(_ root: TreeNode?) -> Bool {
+      var stack: [TreeNode?] = [TreeNode?]()
+      guard let rootNode = root, rootNode != nil else {
+          return true
+      }
+      stack.append(rootNode.left)
+      stack.append(rootNode.right)
+      
+      while !stack.isEmpty {
+          let leftNode = stack.popLast()
+          let rightNode = stack.popLast()
+          if leftNode! == nil && rightNode! == nil {
+              continue
+          }
+
+          guard let left = leftNode, let right = rightNode, left != nil && right != nil && left?.val == right?.val else {
+             return false
+          }
+          stack.append(left?.left)
+          stack.append(right?.right)
+          stack.append(left?.right)
+          stack.append(right?.left)
+      }
+      
+      return true
+  }
+
+    
+}
