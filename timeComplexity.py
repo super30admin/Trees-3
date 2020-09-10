0165ff43ec6077d14908b1074acb5d50bc3687bc
Shriyansh Jain <shriyanshajain@gmail.com>
2020-09-10 12:30:50 -0700

    Create timeComplexity.py

---
+++ timeComplexity.py
@@ -0,0 +1,10 @@
+path sum
+
+time complexity: O(n)
+space complxity: O(max(n,c*(maxdepth)))
+
+
+symmetric tree
+
+time complexity: O(n)
+space complxity: O(1) with recurssion, O(maxdepth) without recurssion
