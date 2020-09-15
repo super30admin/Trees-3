881525870d30f6d99088ff4212015ea82659306a
koushik-kumar <koushikkumarkamala@gmail.com>
2020-09-14 19:12:29 -0500

    Trees-3 Updated Complexities

--- PathSum.java
+++ PathSum.java
@@ -1,5 +1,12 @@
 class PathSum {
-    
+    // Time Complexity:    O(n^2) 
+        // - traverse every element
+        // - For every leaf node, we copy the depth number of elements(from root to leaf path) Path into temp
+        // ==> n leaf nodes * depth of the leaf node
+        // ==> diameter * max depth, where diameter is num of leafnodes, max depth - symtotically max num of nodes for copying
+        // ==> worse case - complete binary tree ==> O(n^2)
+
+    // Space Complexity:   O(n)  - currPath, for storing the path or nodes
     List<List<Integer>> output = new ArrayList<>();
     
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
@@ -21,7 +28,7 @@ class PathSum {
             if(currSum == target){
                 //New List is required since when currPath changes, the list in output will also change
                 // since reference is passed (as value in Java)
-                List<Integer> temp = new ArrayList<>(currPath);
+                List<Integer> temp = new ArrayList<>(currPath); // O(n) to copy into new Array
                 temp.add(root.val);
                 output.add(temp);
             }
