f44b4cd7205c3894c36eb07aa04f5995a51f1454
Juhi Madhwani <jhmadhwa@ncsu.edu>
2021-01-17 00:50:04 -0500

    Completed Problem 2

--- Problem 2.java
+++ Problem 2.java
@@ -51,3 +51,45 @@ class Solution {
         helper(left.right, right.left);
     }
 }
+
+
+
+//ITERATIVE
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        
+        stack.push(root.left);
+        stack.push(root.right);
+        
+        while(!stack.isEmpty())
+        {
+            TreeNode right = stack.pop();
+            TreeNode left = stack.pop();
+            if(left == null && right == null) continue;
+            if(left == null || right == null || left.val != right.val) return false;
+            stack.push(left.left);
+            stack.push(right.right);
+            stack.push(left.right);
+            stack.push(right.left);
+        }
+        return true;
+    }
+}
