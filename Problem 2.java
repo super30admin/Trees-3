dc8119ee136f2c82af8d5b201f5051dcd069c780
Juhi Madhwani <jhmadhwa@ncsu.edu>
2021-01-17 00:41:12 -0500

    Completed Problem 2

---
+++ Problem 2.java
@@ -0,0 +1,53 @@
+//Time Complexity: O(N) where N is the total number of nodes
+//Space Complexity: O(h) where h is the height of the tree and if we consider recursive stack space
+
+//Successfully runs on leetcode: 0 ms
+
+//Approach: We recrusively traverse through the leftmost branch of the left subtree and rightmost branch of the right subtree,
+//compare the values and if at any point, they are not same, we return false, else we go to the right children of left subtree
+//and left children of light subtree in the same recursive way
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValid;
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        isValid = true;
+        helper(root.left, root.right);
+        return isValid;
+    }
+    
+    private void helper(TreeNode left, TreeNode right)
+    {
+        //base
+        if(left == null && right == null)
+            return;
+        
+        
+        //logic
+        if(left == null || right == null || left.val != right.val)
+        {
+            isValid = false;
+            return;
+        }
+        
+        helper(left.left, right.right);
+        
+        helper(left.right, right.left);
+    }
+}
