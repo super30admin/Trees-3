4fefc311d86b3bd81f0127d97ff6f89ca937fb52
HonglaiPeng <honglaipeng007@gmail.com>
2021-06-17 21:34:40 -0400

    Add files via upload

---
+++ 101. Symmetric Tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return helper(root, root);
+    }
+    public boolean helper(TreeNode r1, TreeNode r2){
+        // various conditions
+        if(r1 == null && r2 == null)
+            return true;
+        if(r1 == null || r2 == null)
+            return false;
+        if(r1.val != r2.val)
+            return false;
+       return helper(r1.left, r2.right) && helper(r1.right, r2.left);
+    }
+}
