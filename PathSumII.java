4aa0210c849971eb43234b01c5c82753c4469e2a
VaibhaviDharashivkar97 <54762802+VaibhaviDharashivkar97@users.noreply.github.com>
2021-06-18 20:45:20 -0400

    Create PathSumII.java
    Leetcode problem 113. Path Sum II solved.

---
+++ PathSumII.java
@@ -0,0 +1,39 @@
+// Time Complexity : O(N)
+// Space Complexity : O(H=N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : I tried to solve this question with helper(root, currSum) but couldn't do it on my own.
+
+class Solution {
+    List<List<Integer>> paths;
+    List<Integer> curr_path;
+    int target;
+    int currSum;
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        this.paths = new ArrayList<>();
+        this.curr_path = new ArrayList<>();
+        this.target = targetSum;
+        this.currSum = 0;
+        helper(root);
+        return paths;
+    }
+    
+    private void helper(TreeNode root){
+        //base
+        if(root == null)
+            return;
+         
+        //logic
+        curr_path.add(root.val);
+        currSum+=root.val;
+       
+        if(root.left == null && root.right == null && currSum == target)
+            paths.add(new ArrayList<>(curr_path));
+            
+        helper(root.left);
+        
+        helper(root.right);
+        
+        curr_path.remove(curr_path.size() - 1);
+        currSum -= root.val;
+    }
+}
