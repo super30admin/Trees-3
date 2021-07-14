0194e30eb9a8c272a779aea2f287f2a20306742e
jisakp7 <kavya.pathi@jbhunt.com>
2021-07-14 18:59:17 -0500

    SymmetricTree.java Recursive approach PathSumii.java

--- java/SymmetricTree.java
+++ java/SymmetricTree.java
@@ -1,7 +1,35 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
+Recursive approach to check root values equality and each value of mirror (left of left tree should be equal to right of right tree) is equal.
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2){
+        if(t1==null && t2==null) return true;
+        if(t1==null || t2==null) return false;
+        return (t1.val==t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+
+    }
+}
