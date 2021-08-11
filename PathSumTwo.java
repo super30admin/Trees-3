5299b56b3df3117211144c08e9e5ccd28a504320
Tanreet Dhaliwal <tdhali@uwo.ca>
2021-08-11 16:05:37 -0700

    updated trees3

--- PathSumTwo.java
+++ PathSumTwo.java
@@ -1,13 +1,35 @@
-class PathSumTwo {
+class Solution {
+    int target;
+    List<List<Integer>> result;
+
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+
+        result = new ArrayList<>();
+        target = targetSum;
+        helper(root, 0, new ArrayList<>());
+        return result;
 
     }
 
-    private void recurseTree(TreeNode root, int sum, <List<Integer> path, List<List<Integer>> pathList) {
-        if (nodes == null){
-            return;
+    private void helper(TreeNode root, int currSum, List<Integer> path) {
+
+        //base case
+        if (root == null) return;
+
+        currSum += root.val;
+        //action
+        path.add(root.val);
+        helper(root.left, currSum, path);
+
+        if (root.left == null && root.right == null) {
+            if (target == currSum) {
+                result.add(new ArrayList<>(path));
+            }
         }
 
-        path.add(node.val);
+        //recurse
+        helper(root.right, currSum, path);
+        //backtrack
+        path.remove(path.size() - 1);
     }
 }
