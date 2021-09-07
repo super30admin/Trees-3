47427d76c8522cdeff5a4e7645050b158209d010
balasudip <balasudip6@gmail.com>
2021-09-06 18:48:00 -0700

    Completed Sprint Trees-3

---
+++ Problem48_SymmetricTree.java
@@ -0,0 +1,17 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null)
+            return true;
+        if(t1 == null || t2 == null)
+            return false;
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+}
