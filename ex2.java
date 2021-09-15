b8a0114b7238d926fe8b3c3054a239cb7f4c0bfe
shilpagowda812 <33579765+shilpagowda812@users.noreply.github.com>
2021-09-15 00:11:32 -0400

    Trees-3 Completed, Please review

---
+++ ex2.java
@@ -0,0 +1,25 @@
+class Solution {
+   public boolean isSymmetric(TreeNode root) {
+	if (root == null)
+		return true;
+	return isSymmetric(root.left, root.right);
+}
+ 
+public boolean isSymmetric(TreeNode l, TreeNode r) {
+	if (l == null && r == null) {
+		return true;
+	} else if (r == null || l == null) {
+		return false;
+	}
+ 
+	if (l.val != r.val)
+		return false;
+ 
+	if (!isSymmetric(l.left, r.right))
+		return false;
+	if (!isSymmetric(l.right, r.left))
+		return false;
+ 
+	return true;
+}
+}
