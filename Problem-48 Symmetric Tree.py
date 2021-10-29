ea27fc946ff3c9e3cc58dbc5ad4025adf1b6a6a8
Adhish Shrivastava <adhish70@gmail.com>
2021-10-29 14:46:11 -0400

    Problem48 done

--- Problem-48 Symmetric Tree.py
+++ Problem-48 Symmetric Tree.py
@@ -1,7 +1,7 @@
 # 101. Symmetric Tree
 # https://leetcode.com/problems/symmetric-tree/
 
-# Recursive Approach
+# Recursive Approach - 1
 # Time complexiety: O(n)
 # Space complexiety: O(n)
 class Solution:
@@ -24,9 +24,56 @@ class Solution:
         return self._helper(root.left, [], True) == self._helper(root.right, [], False)
 
 
-# Iterative Approach
-# Time complexiety: 
-# Space complexiety: 
+# Recursive Approach - 2
+# Time complexiety: O(n)
+# Space complexiety: O(n)
 class Solution:
+    def _helper(self, l, r):
+        if l == None and r == None:
+            return True
+        
+        if l == None or r == None:
+            return False
+        
+        if l.val != r.val:
+            return False
+        
+        return self._helper(l.left,r.right) and self._helper(l.right,r.left)       
+    
     def isSymmetric(self, root) -> bool:
-        pass
+        if not root:
+            return False
+        
+        return self._helper(root.left,root.right)
+
+
+# Iterative Approach
+# Time complexiety: O(n)
+# Space complexiety: O(n)
+class Solution:   
+    def isSymmetric(self, root) -> bool:
+        if not root:
+            return False
+        
+        stack = []
+        stack.append(root.left)
+        stack.append(root.right)
+        
+        while stack:
+            r = stack.pop()
+            l = stack.pop()
+            
+            if l == None and r == None:
+                continue
+            
+            if l == None or r == None:
+                return False
+            
+            if l.val != r.val:
+                return False
+            
+            stack.append(l.left)
+            stack.append(r.right)
+            stack.append(l.right)
+            stack.append(r.left)
+        return True
