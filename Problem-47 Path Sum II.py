8543a31193172a8d976e7a6bcbf44ae3a73c490c
Adhish Shrivastava <adhish70@gmail.com>
2021-10-29 12:03:10 -0400

    Problem48 done

--- Problem-47 Path Sum II.py
+++ Problem-47 Path Sum II.py
@@ -8,17 +8,27 @@
 #         self.left = left
 #         self.right = right
 
-# Recursive Approach
-# Time complexiety: 
-# Space complexiety: 
-class Solution:
-    def pathSum(self, root, targetSum):
-        pass
-        
-
 # Iterative Approach
-# Time complexiety: 
-# Space complexiety: 
-class Solution:
+# Time complexiety: O(n)
+# Space complexiety: O(n)
+class Solution:    
     def pathSum(self, root, targetSum):
-        pass
+        if not root:
+            return []
+        
+        res = []
+        
+        stack = [(root,[root.val], targetSum-root.val)]
+        
+        while stack:
+            root, eleList, t = stack.pop()
+            
+            if root.left == None and root.right == None and t == 0:
+                res.append(eleList)
+            
+            if root.right:
+                stack.append((root.right,eleList+[root.right.val],t-root.right.val))
+            
+            if root.left:
+                stack.append((root.left,eleList+[root.left.val],t-root.left.val))
+        return res
--- Problem-48 Symmetric Tree.py
+++ Problem-48 Symmetric Tree.py
@@ -2,11 +2,26 @@
 # https://leetcode.com/problems/symmetric-tree/
 
 # Recursive Approach
-# Time complexiety: 
-# Space complexiety: 
+# Time complexiety: O(n)
+# Space complexiety: O(n)
 class Solution:
+    def _helper(self, root, s, isLeft):
+        if not root:
+            s.append(-1000)
+            return s
+        
+        if isLeft:
+            s.append(root.val)
+            self._helper(root.left,s, True)
+            self._helper(root.right,s, True)
+        else:
+            s.append(root.val)
+            self._helper(root.right,s, False)
+            self._helper(root.left,s, False)
+        return s
+    
     def isSymmetric(self, root) -> bool:
-        pass
+        return self._helper(root.left, [], True) == self._helper(root.right, [], False)
 
 
 # Iterative Approach
