11723652f9df01e0a0d55ed2e0c6ab0af084d9f7
Adhish Shrivastava <adhish70@gmail.com>
2021-10-28 21:42:08 -0400

    Initial commit

---
+++ Problem-47 Path Sum II.py
@@ -0,0 +1,24 @@
+# 113. Path Sum II
+# https://leetcode.com/problems/path-sum-ii/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Recursive Approach
+# Time complexiety: 
+# Space complexiety: 
+class Solution:
+    def pathSum(self, root, targetSum):
+        pass
+        
+
+# Iterative Approach
+# Time complexiety: 
+# Space complexiety: 
+class Solution:
+    def pathSum(self, root, targetSum):
+        pass
---
+++ Problem-48 Symmetric Tree.py
@@ -0,0 +1,17 @@
+# 101. Symmetric Tree
+# https://leetcode.com/problems/symmetric-tree/
+
+# Recursive Approach
+# Time complexiety: 
+# Space complexiety: 
+class Solution:
+    def isSymmetric(self, root) -> bool:
+        pass
+
+
+# Iterative Approach
+# Time complexiety: 
+# Space complexiety: 
+class Solution:
+    def isSymmetric(self, root) -> bool:
+        pass
