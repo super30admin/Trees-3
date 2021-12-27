1251bd4efb4e5d27f43030c275a8eec58f830680
shkamtek <shkamtek@cisco.com>
2021-12-27 01:03:40 -0800

    add solution

--- PathSumII.java
+++ PathSumII.java
@@ -1,87 +1,36 @@
-// Time Complexity : O(n)
+// Time Complexity : O(n^2)
 // Space Complexity : O(h)
 
-// Bottom up
+// Backtracking
 public class PathSumII {
-    public boolean isSymmetric(TreeNode root) {
-        if(root == null)
-            return true;
-        return dfs(root.left, root.right);
-    }
-    
-    
-    private boolean dfs(TreeNode left, TreeNode right)
-    {
-        // base
-        if(left == null && right == null)
-            return true;
-        if(left == null || right == null || left.val != right.val)
-        {
-            return false;
-        }
+    List<List<Integer>> result;
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        result = new ArrayList<>();
         
-        // logic
-        return dfs(left.left, right.right) && dfs(left.right, right.left);
-    }
-}
-// DFS
-public class PathSumII {
-    boolean isSymmetric = true;
-    public boolean isSymmetric(TreeNode root) {
         if(root == null)
-            return isSymmetric;
-        dfs(root.left, root.right);
-        return isSymmetric;
+            return result;
+        
+        dfs(root, targetSum, 0, new ArrayList<>());
+        
+        return result;
     }
     
-    
-    private void dfs(TreeNode left, TreeNode right)
+    private void dfs(TreeNode root, int targetSum, int currSum , List<Integer> path)
     {
-        // base
-        if(left == null && right == null)
+        // base case
+        if(root == null)
             return;
-        if(left == null || right == null || left.val != right.val)
+        
+        // recursive case
+        currSum += root.val;
+        path.add(root.val);
+        if(root.left == null && root.right == null && sum == targetSum)
         {
-            isSymmetric = false;
-            return ;
+            result.add(new ArrayList<>(path));
         }
         
-        // logic
-        if(isSymmetric)
-            dfs(left.left, right.right);
-        if(isSymmetric) // minor optimization
-            dfs(left.right, right.left);
-
-    }
-}
-
-// Iterative BFS
-public class PathSumII {
-    public boolean isSymmetric(TreeNode root) {
-        if(root == null)
-            return true;
-        
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root.left);
-        q.offer(root.right);
-        
-        while(!q.isEmpty())
-        {
-            TreeNode left = q.poll();
-            TreeNode right = q.poll();
-            
-            if(left == null && right == null)
-                continue; // in lower levels there can be more nodes and we need to evaluate all pairs on that level
-            
-            if(left == null || right == null || left.val != right.val)
-                return false;
-            
-            // add nodes for next level
-            q.offer(left.left);
-            q.offer(right.right);
-            q.offer(left.right);
-            q.offer(right.left);
-        }
-        return true;
+        dfs(root.left, targetSum, currSum, path);
+        dfs(root.right, targetSum, currSum, path);
+        path.remove(path.size()-1);
     }
 }
