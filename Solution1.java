024a293ef05d7e10a89aa404147b990aea5cde9a
Parikshit Narang <parikshit3097@gmail.com>
2022-03-07 00:51:10 -0500

    completed trees-3 : please review

---
+++ Solution1.java
@@ -0,0 +1,19 @@
+public class Solution {
+    private List<List<Integer>> resultList = new ArrayList<List<Integer>>();
+    
+    public void pathSumInner(TreeNode root, int sum, Stack<Integer>path) {
+        path.push(root.val);
+        if(root.left == null && root.right == null)
+            if(sum == root.val) resultList.add(new ArrayList<Integer>(path));
+        if(root.left!=null) pathSumInner(root.left, sum-root.val, path);
+        if(root.right!=null)pathSumInner(root.right, sum-root.val, path);
+        path.pop();
+    }
+    
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if(root==null) return resultList;
+        Stack<Integer> path = new Stack<Integer>();
+        pathSumInner(root, sum, path);
+        return resultList;
+    }
+}
---
+++ Solution2.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/*
+    Time Complexity : O(n)
+    Space Complexity : O(n)
+*/
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while(!queue.isEmpty()){
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if(left == null && right == null) continue;
+            if(left == null || right == null) return false;
+            if(left.val != right.val) return false;
+            queue.offer(left.left);
+            queue.offer(right.right);
+            queue.offer(left.right);
+            queue.offer(right.left);
+            
+        }
+        return true;
+        
+    }
+}
