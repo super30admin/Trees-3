b1b151f67a284917b20fd4918c8a1ce023430f5c
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-04-25 21:53:24 -0700

    Add files via upload

---
+++ SymmetricTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class SymmetricTree
+    {
+        /*
+         * T.C: O(N) travers through all elements of the tree
+         * S.C: O(h) hight of the tree
+         */
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return true;
+
+            //isValid=true;
+
+            return DFS(root.left, root.right);
+
+            //return isValid;
+        }
+
+        private bool DFS(TreeNode rootLeft, TreeNode rootRight)
+        {
+            if (rootLeft == null && rootRight == null) return true;
+
+            if (rootLeft == null || rootRight == null || rootLeft.val != rootRight.val)
+            {
+                return false;
+            }
+
+            if (DFS(rootLeft.left, rootRight.right) == false)
+                return false;
+
+
+
+            return DFS(rootLeft.right, rootRight.left);
+        }
+    }
+}
