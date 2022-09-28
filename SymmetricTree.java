43ce77044dc296db65faad4c37941bccce1de301
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-09-27 22:51:52 -0700

    Symmetric Tree

---
+++ SymmetricTree.java
@@ -0,0 +1,58 @@
+// Time Complexity : O(n) where n = number of elements in tree
+// Space Complexity : O(h) where h = height of the tree
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+// Your code here along with comments explaining your approach
+
+//101. Symmetric Tree (Easy) - https://leetcode.com/problems/symmetric-tree/
+//Approach-1: Using the DFS - treating single tree as two different trees
+//Overall Time Complexity: O(N)
+//Overall Space Complexity: O(H) - Height of the recursion stack - here height of the tree
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+
+        //Left and right part of tree as two different trees
+        return dfs(root.left, root.right); 
+    }
+
+    public boolean dfs(TreeNode left, TreeNode right){
+        if(left == null && right == null) return true;
+        if(left == null || right == null || left.val != right.val) return false;
+
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
+}
+
+
+//Approach-2: Using the BFS
+//Overall Time Complexity: O(N)
+//Overall Space Complexity: O(N) - leaf level
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root.left);
+        q.add(root.right);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode left = q.poll();
+                TreeNode right = q.poll();
+
+                if(left == null && right == null) continue;
+                if(left == null || right == null || left.val != right.val) return false;
+
+                q.add(left.left);
+                q.add(right.right);
+                q.add(left.right);
+                q.add(right.left);
+            }
+        }
+
+        return true;
+    }
+}
