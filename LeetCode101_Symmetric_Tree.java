baa2e1919f96f9bd30727ed314c456f876b265ff
khushbukareliya07 <khushbu.karelia@gmail.com>
2022-10-10 15:24:08 -0500

    Leetcode 101 - new approach added

--- LeetCode101_Symmetric_Tree.java
+++ LeetCode101_Symmetric_Tree.java
@@ -162,3 +162,55 @@ class Solution {
         inOrder(root.right);
     }
 }
+
+//approach - 3
+//iterativve
+//BFS approach - level by level traversal
+//add left's left and right's right, and check if they are equal, if not return false, 
+//else check left's right and right's left and same logic!
+//TC- O(n)
+//SC - O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        
+        if(root == null) return true;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        q.add(root.left);
+        q.add(root.right);
+        
+        while(!q.isEmpty())
+        {
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+            
+            if(left == null && right == null) continue;
+            
+            if(left == null  || right == null || left.val != right.val) return false;
+            
+            //following order matters for checking symmetricity. 
+            q.add(left.left);
+            q.add(right.right);
+            q.add(left.right);
+            q.add(right.left);
+        }
+        return true;
+    }
+}
