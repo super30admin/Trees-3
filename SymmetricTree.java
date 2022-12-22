94b86a00ef6abfa18c81dbb4f5a50b100f77365e
TarunGambhi <118712515+TarunGambhi@users.noreply.github.com>
2022-12-22 15:20:40 +0530

    Update SymmetricTree.java

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -1,6 +1,7 @@
 //TC : o(n)
 //SC : o(h) (recursive stack space)
 //https://leetcode.com/problems/symmetric-tree/
+//dfs
 class Solution {
     boolean flag = true;
     public boolean isSymmetric(TreeNode root) {
@@ -21,4 +22,26 @@ class Solution {
         if(flag)
             dfs(left.right, right.left);
     }
+}
+//bfs
+class Solution {
+    boolean flag = true;
+    public boolean isSymmetric(TreeNode root) {
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root.left);
+        q.add(root.right);
+        while(!q.isEmpty()){
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+            if(left == null && right ==null) continue;
+            if(left ==null || right == null || left.val != right.val){
+                return false;
+            }
+            q.add(left.left); q.add(right.right);
+            q.add(left.right); q.add(right.left);
+        }
+        return true;
+    }
+    
 }
