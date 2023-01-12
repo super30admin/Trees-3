b49a575adba6f59d66fdfb3a6ea5ef97195e7979
rajesh <rajeshdm9@gmail.com>
2023-01-11 19:30:04 -0800

    all problems done

---
+++ mirror_tree.py
@@ -0,0 +1,37 @@
+# Time Complexity :
+# O(N)  - Size of the Truee
+
+# Space Complexity :
+# O(N)
+
+# Did this code successfully run on Leetcode :
+#Yes
+
+#We do a inorder traversal in two different ways - (left, root, right) and (right,root, left). If both left and right value are same, and the left subtree and right subtree are mirrors,then we return True. Else we return False
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.SymmetricTraverse(root,root)
+
+    def SymmetricTraverse(self,leftRoot,rightRoot):
+        if leftRoot == None and rightRoot == None :
+            return True
+        if leftRoot != None and rightRoot == None :
+            return False
+        if leftRoot == None and rightRoot != None :
+            return False
+
+        left_tree = self.SymmetricTraverse(leftRoot.left,rightRoot.right)
+        if not left_tree :
+            return False
+        if leftRoot.val != rightRoot.val :
+            return False
+        right_tree = self.SymmetricTraverse(leftRoot.right,rightRoot.left)
+
+        return  left_tree and right_tree 
---
+++ two_sum_2.py
@@ -0,0 +1,41 @@
+# Time Complexity :
+# O(N)  - Size of the Truee
+
+# Space Complexity :
+# O(N)
+
+# Did this code successfully run on Leetcode :
+#Yes
+
+#We go through the tree in a  pre order manner. Whenever we hit a leaf node, we check if the path until then 
+#sums to the target sum. If it does, we add the path to the set of results
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.lists = []
+
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        self.targetSum = targetSum
+        self.traverse(root,0,[])
+        return self.lists
+
+    def traverse(self,root,currSum,curr_history):
+        if root == None :
+            return
+        
+        currSum = currSum + root.val
+        curr_history.append(root.val)
+        if root.left == None and root.right == None :
+            if currSum == self.targetSum:
+                self.lists.append(curr_history[:])
+
+        self.traverse(root.left,currSum,curr_history)
+        self.traverse(root.right,currSum,curr_history)
+        curr_history.pop()
