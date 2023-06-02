2fa8264eee03da2c04bfaa2dae6d3f9bbd9ea110
PRAYAG-PATEL-1998 <90693541+PRAYAG-PATEL-1998@users.noreply.github.com>
2023-06-01 17:49:40 -0700

    Update Sample.java
    Trees-3 completed

--- Sample.java
+++ Sample.java
@@ -1,7 +1,59 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+class Solution {
+    List<List<Integer>> result;
+    int tar;
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        result = new ArrayList<>();
+        if(root == null) return result;        
+        tar = targetSum;
+        DFS(root,0,new ArrayList<>());
+        return result;
+    }
+    
+    private void DFS(TreeNode root, int sum, List<Integer> path){
+        //base
+        if(root == null) return;
+        
+        //logic
+            //Action
+        path.add(root.val);
+        sum += root.val;
+        
+            //Recursion
+        DFS(root.left, sum, path);
+        
+        if((root.left == null && root.right == null) && (sum == tar)){
+            
+            result.add(new ArrayList<>(path));
+        }     
+        
+        DFS(root.right, sum, path); 
+            //Backtrack
+        path.remove(path.size()-1);
+    }
+    
+    
+}
 
 
-// Your code here along with comments explaining your approach
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null)return true;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root.left);
+        st.push(root.right);
+        while(!st.isEmpty()){
+            TreeNode right = st.pop();
+            TreeNode left = st.pop();
+            if(left == null && right == null)continue;
+            if(left == null && right !=null)return false;
+            if(left != null && right ==null)return false;
+            if(left.val != right.val)return false;
+            st.push(left.left);
+            st.push(right.right);
+            st.push(left.right);
+            st.push(right.left);
+
+        }
+        return true; 
+    }
+}//iterative solution
