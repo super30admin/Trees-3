2c8507972cb7d27f2d5ae4ad8b5669fd8451aed8
OmkarDedge <123588944+OmkarDedge@users.noreply.github.com>
2023-06-20 13:53:07 -0700

    Add files via upload

---
+++ SymmetricTree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// Time complexity o(n) Space complexity o(h)
+class Solution {
+    
+    public boolean isSymmetric(TreeNode root) {
+       if(root == null) return true;
+       return helper(root.left, root.right); 
+    }
+    public boolean helper(TreeNode left, TreeNode right){
+        if(left == null || right==null){
+            return left==right;
+        }
+        if(left.val!=right.val) return false;
+        return helper(left.left,right.right) && helper(left.right,right.left);
+    }
+}
