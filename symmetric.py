ccb6cd1225dfe1eb91b61753f9a52b289f9d9deb
DEEPIKA <170031561@kluniversity.in>
2023-07-17 20:32:31 -0500

    Completed Trees-3 - please review

---
+++ symmetric.py
@@ -0,0 +1,17 @@
+#TC - O(N)
+#SC - O(N)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def dfs(left,right):
+            if not left and not right:
+                return True 
+            if not left or not right:
+                return False 
+            return left.val == right.val and (dfs(left.left, right.right) and dfs(left.right, right.left))
+        return dfs(root.left,root.right)
