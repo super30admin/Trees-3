dde714232e4134a3d2214448aa4ea230e565fcce
Jaissica <jaissica.hora@gmail.com>
2023-10-01 21:56:33 -0400

    Create Create Path Sum II-113-Recursive-Preorder.java

---
+++ Create Path Sum II-113-Recursive-Preorder.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ // Time Complexity: O(n^2) where n times we are calling n copies in array
+ //Space Complexity: O(n^2)  
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        if(root==null)
+        {
+            return new ArrayList<>();
+        }
+        result = new ArrayList<>();
+        inorder(root, 0, targetSum, new ArrayList<>());
+        return result;
+    }
+
+    private void inorder(TreeNode root, int currSum, int targetSum, ArrayList<Integer> path)
+    {
+        if(root==null)
+        {
+            return;
+        }
+
+        
+        if(root.left==null && root.right==null)
+        {
+            if(targetSum == currSum+root.val)
+            {
+                result.add(path);
+            }
+        }
+        path.add(root.val);
+        inorder(root.left, currSum+root.val, targetSum, new ArrayList<>(path));
+        inorder(root.right, currSum+root.val, targetSum, new ArrayList<>(path));
+    }
+}
