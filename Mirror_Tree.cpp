172536225c8786bb093bb7c91214b59986cb4993
Yatish Koul <36380820+yatishkoul@users.noreply.github.com>
2023-10-26 15:08:48 -0700

    Create Mirror_Tree.cpp

---
+++ Mirror_Tree.cpp
@@ -0,0 +1,23 @@
+
+ //dfs Solution
+class Solution {
+    bool flag;
+public:
+    bool isSymmetric(TreeNode* root) {
+        flag = true;
+        dfs(root->left,root->right);
+        return flag;
+    }
+private:
+    void dfs(TreeNode* left_child, TreeNode* right_child){
+     //base
+        if(left_child==NULL && right_child==NULL)return;
+     //logic
+        if(left_child==NULL || right_child==NULL ||                     left_child->val!=right_child->val){             
+            flag=false;
+            return;
+        }
+        dfs(left_child->left, right_child->right);
+        dfs(left_child->right, right_child->left);
+    }    
+};
