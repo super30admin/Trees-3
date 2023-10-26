7e8913b300bf57838b5e7a135c794833728b799c
Yatish Koul <36380820+yatishkoul@users.noreply.github.com>
2023-10-26 15:35:19 -0700

    Update Mirror_Tree.cpp

--- Mirror_Tree.cpp
+++ Mirror_Tree.cpp
@@ -52,3 +52,27 @@ private:
 };
 
 */
+/*
+ //bfs solution
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root->left);
+        q.push(root->right);
+        while(!q.empty()){
+            TreeNode* left = q.front();
+            q.pop();
+            TreeNode* right = q.front();
+            q.pop();
+            if(left== NULL && right == NULL)continue;
+            if(left==NULL || right == NULL || left->val!=right->val)return false;
+            q.push(left->left);
+            q.push(right->right);
+            q.push(left->right);
+            q.push(right->left);
+        }
+        return true;
+    }
+};
+*/
