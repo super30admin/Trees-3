4aff4917aed3ef588df93716971053fb0a749edd
Siri-isha <150191332+Siri-isha@users.noreply.github.com>
2023-12-09 19:58:30 -0400

    Done SymmetricTree.java

---
+++ SymmetricTree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return false;
+
+        return check(root.left, root.right);
+        
+    }
+    private boolean check(TreeNode left, TreeNode right){
+        if(left==null && right ==null) return true;
+        if((left == null && right != null) ||(right == null && left != null)) return false;
+        if(left.val != right.val) return false;
+        
+        return check(left.left, right.right) && check(left.right, right.left);
+    }
+}
