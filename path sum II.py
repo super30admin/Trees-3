381416aae4003ad31841ee13042d1a1ba1ec8894
ruutujaa <ghogare.r@northeastern.edu>
2024-02-29 15:43:55 -0500

    Done with Trees-3

---
+++ path sum II.py
@@ -0,0 +1,31 @@
+#time complexity: O(n)
+#space complexity: O(h)
+
+class Solution:
+    
+    def recurseTree(self, node, remainingSum, pathNodes, pathsList):
+        
+        if not node:
+            return 
+        
+        # Add the current node to the path's list
+        pathNodes.append(node.val)
+        
+        # Check if the current node is a leaf and also, if it
+        # equals our remaining sum. If it does, we add the path to
+        # our list of paths
+        if remainingSum == node.val and not node.left and not node.right:
+            pathsList.append(list(pathNodes))
+        else:    
+            # Else, we will recurse on the left and the right children
+            self.recurseTree(node.left, remainingSum - node.val, pathNodes, pathsList)
+            self.recurseTree(node.right, remainingSum - node.val, pathNodes, pathsList)
+            
+        # We need to pop the node once we are done processing ALL of it's
+        # subtrees.
+        pathNodes.pop()    
+    
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        pathsList = []
+        self.recurseTree(root, sum, [], pathsList)
+        return pathsList
